867583cc0db10a746708a4eda8b928aa453ace03
mercymurigi27 <mercy.murigi@student.moringaschool.com>
2022-03-06 23:40:29 +0300

    create function

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-const myGender = document.myform.gender.value;
 //get the value of gender form
 const submitBtn = document.getElementById("submitbtn");
 //give submit button a variable
 submitBtn.addEventListener("click",(e)=>{
     e.preventDefault();
-    
+    const myGender = document.myform.gender.value;
     const khanName = document.getElementById("khanname");
 //get element of the name to be assigned
     const myBirthday = document.getElementById("birthday").value;
@@ -15,6 +14,17 @@ submitBtn.addEventListener("click",(e)=>{
     var fAkhanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     var mAkhanNames = ["Kwasi", "Kwadwo","Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
-    
-
+    if(myBirthday==""){
+        alert ("Input your birthday date");
+        
+    }else if(myGender==""){
+        alert("Choose your gender");
+    }else{
+        if(myGender=="male"){
+            khanName.innerHTML = `Congratulations!!! Your khan name is ${mAkhanNames[myDay]}`
+        }else{
+            khanName.innerHTML = `Congratulations!!! Your khan name is ${fAkhanNames[myDay]}`
+        }
+ 
+    }
 })
