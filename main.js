f4f99c8a63ce1224bd763fd70ffd4035f33aee3a
mercymurigi27 <mercy.murigi@student.moringaschool.com>
2022-03-06 22:49:28 +0300

    defined variables

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-
 const myGender = document.myform.gender.value;
-
-
-
+//get the value of gender form
 const submitBtn = document.getElementById("submitbtn");
-
+//give submit button a variable
 submitBtn.addEventListener("click",(e)=>{
     e.preventDefault();
+    
     const khanName = document.getElementById("khanname");
-
+//get element of the name to be assigned
     const myBirthday = document.getElementById("birthday").value;
+    
     var newBirthday = new Date(myBirthday); 
-console.log (newBirthday)
+    var myDay = newBirthday.getDay()
+    
+    var fAkhanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var mAkhanNames = ["Kwasi", "Kwadwo","Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+    
+
 })
